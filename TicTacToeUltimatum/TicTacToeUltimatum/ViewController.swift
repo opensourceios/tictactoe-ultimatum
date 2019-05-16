4132b995936300e9e1d58993afe504999369417e
Maksim Khrapov <mkhrapov@users.noreply.github.com>
2019-05-16 18:07:18 -0500

    iOS app scaffolding

---
+++ TicTacToeUltimatum/TicTacToeUltimatum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeUltimatum
+//
+//  Created by Maksim Khrapov on 5/16/19.
+//  Copyright © 2019 Maksim Khrapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
